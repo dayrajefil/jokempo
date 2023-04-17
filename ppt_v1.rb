621987a4ab2c4a3c44c37f18ac9849bcecc3450d
dayrajefil <88631670+dayrajefil@users.noreply.github.com>
2023-04-17 13:19:10 -0300

    Update ppt_v1.rb

--- ppt_v1.rb
+++ ppt_v1.rb
@@ -2,12 +2,7 @@ puts "Bem-vindo ao jogo de Pedra, Papel e Tesoura!"
 
 options = ['pedra', 'papel', 'tesoura']
 
-print '''==========
-0 pedra
-1 papel
-2 tesoura
-==========
-'''
+puts "(0) pedra \n(1) papel \n(2) tesoura"
 
 print 'escolha uma das opções (0, 1 ou 2): '
 option_user = gets.chomp.to_i
@@ -16,7 +11,9 @@ option_pc = rand(0..2)
 
 if option_user == option_pc
     result = 'empatou'    
-elsif option_user == 0 && option_pc == 2 || option_user == 1 && option_pc == 0 || option_user == 2 && option_pc == 0
+elsif ( option_user == 0 && option_pc == 2 || 
+        option_user == 1 && option_pc == 0 ||
+        option_user == 2 && option_pc == 1 )
     result = 'ganhou'
 else
     result = 'perdeu'
